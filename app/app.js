26037d248a11a36b33c85f38ba4e8027b635cad7
aaronalayo <aaro0186@stud.kea.dk>
2020-08-21 16:23:06 +0200

    Added html to email

--- app/app.js
+++ app/app.js
@@ -6,6 +6,9 @@ const expression = require("./checkMail");
 const config = process.env;
 const illegalChar = require("./illegalChar.js");
 const helmet = require("helmet");
+const fs = require('fs');
+const { promisify } = require('util');
+const readFile = promisify(fs.readFile);
 
 app.use(helmet());
 
@@ -21,8 +24,8 @@ app.get("/test", async (req, res) => {
 });
 
 app.post("/email", async (req, res) => {
-  const { subject, body, recipient } = req.body;
-
+  const { subject, body, recipient} = req.body;
+  
   if (subject.length == 0 || subject.length > 100) {
     res.status(411).send("Subject length must be between 1 - 100 characters.");
   } else if (illegalChar.test(subject) == false) {
@@ -44,7 +47,8 @@ app.post("/email", async (req, res) => {
       from: config.MAILUSER,
       to: recipient,
       subject: subject,
-      text: body,
+      text: body
+
     };
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
@@ -58,6 +62,53 @@ app.post("/email", async (req, res) => {
     });
   }
 });
+
+app.post("/email_template", async (req, res) => {
+  const { subject, body, recipient, mailData } = req.body;
+  
+  if (subject.length == 0 || subject.length > 100) {
+    res.status(411).send("Subject length must be between 1 - 100 characters.");
+  } else if (illegalChar.test(subject) == false) {
+    res
+      .status(411)
+      .send({ message: "The email subject has illegal characters" });
+  } else if (body.length == 0 || body.length > 1000) {
+    res
+      .status(411)
+      .send("Email body length must be between 1 - 1000 charachters.");
+  } else if (illegalChar.test(body) == false) {
+    res
+      .status(411)
+      .send({ message: "The email body contains illegal characters" });
+  } else if (expression.test(String(recipient).toLowerCase()) == false) {
+    res.status(411).send("Check email address");
+  } else {
+    var emailText = await readFile('mail_templates/simple_alert.html', 'utf8');
+    emailText = emailText.replace(/{name}/g, mailData.name);
+    emailText = emailText.replace(/{alarm_name}/g, mailData.alarm_name);
+    emailText = emailText.replace(/{alarm_link}/g, mailData.alarm_link);
+    
+    const mailOptions = {
+      from: config.MAILUSER,
+      to: recipient,
+      subject: subject,
+      text: body,
+      html: emailText
+
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        res.status(400).send({
+          message: "Could not send email, got the following error :",
+          error,
+        });
+      } else {
+        res.status(200).send(info);
+      }
+    });
+  }
+});
+
 
 const port = process.env.PORT ? process.env.PORT : 2000;
 server.listen(port, (error) => {
