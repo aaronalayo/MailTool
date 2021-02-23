b9088d667d441162e3c6e4a3575f2f443d8aef10
otopp <ot@upsmarting.com>
2021-02-23 07:32:37 +0100

    New image version

--- build_tag_push.sh
+++ build_tag_push.sh
@@ -1,3 +1,3 @@
 docker-compose build
-docker tag ups_mail_api:local gcr.io/upsmarting/ups_mail_api:v1.0.9
-docker push gcr.io/upsmarting/ups_mail_api:v1.0.9
+docker tag ups_mail_api:local gcr.io/upsmarting/ups_mail_api:v1.0.11
+docker push gcr.io/upsmarting/ups_mail_api:v1.0.11
